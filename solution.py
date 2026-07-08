ce7573239f4ec0cc8697154827d81031596bfae1
gurthuledhu2-crypto <gurthuledhu2@gmail.com>
2026-07-08 11:20:10 +0530

    Create double_number function stub
    Added a function stub for doubling a number.

---
+++ solution.py
@@ -0,0 +1,6 @@
+# Lab 1: Double the Number
+# Instructions: Complete the function below so it returns double the input number x.
+
+def double_number(x):
+    # TODO: Write your code below this line
+    pass
